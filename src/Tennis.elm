8cf6b7f649080b753a39a8a71b9efb4df2abda22
Peter Albert <peter.albert@data2impact.com>
2020-10-08 15:44:15 +0200

    Implemented game logic

--- src/Tennis.elm
+++ src/Tennis.elm
@@ -1,42 +1,143 @@
 module Tennis exposing (..)
 
+import Html exposing (a)
 
-type alias Game =
-    ( Int, Int )
+
+type Game
+    = Ongoing ( Int, Int )
+    | Deuce
+    | AdvantagePlayer1
+    | AdvantagePlayer2
+    | WinPlayer1
+    | WinPlayer2
 
 
 init : Game
 init =
-    ( 0, 0 )
+    Ongoing ( 0, 0 )
 
 
 player1Scores : Game -> Game
-player1Scores ( first, second ) =
-    ( first + 1, second )
+player1Scores game =
+    case game of
+        Ongoing ( first, second ) ->
+            Ongoing ( first + 1, second ) |> checkDeuce |> checkGameEnd
+
+        Deuce ->
+            AdvantagePlayer1
+
+        AdvantagePlayer1 ->
+            WinPlayer1
+
+        AdvantagePlayer2 ->
+            Deuce
+
+        WinPlayer1 ->
+            WinPlayer1
+
+        WinPlayer2 ->
+            WinPlayer2
 
 
 player2Scores : Game -> Game
-player2Scores ( first, second ) =
-    ( first, second + 1 )
+player2Scores game =
+    case game of
+        Ongoing ( first, second ) ->
+            Ongoing ( first, second + 1 ) |> checkDeuce |> checkGameEnd
+
+        Deuce ->
+            AdvantagePlayer2
+
+        AdvantagePlayer1 ->
+            Deuce
+
+        AdvantagePlayer2 ->
+            WinPlayer2
+
+        WinPlayer1 ->
+            WinPlayer1
+
+        WinPlayer2 ->
+            WinPlayer2
+
+
+checkDeuce : Game -> Game
+checkDeuce game =
+    case game of
+        Ongoing ( 3, 3 ) ->
+            Deuce
+
+        _ ->
+            game
+
+
+checkGameEnd : Game -> Game
+checkGameEnd game =
+    case game of
+        Ongoing ( a, b ) ->
+            if a > 3 && a > b + 1 then
+                WinPlayer1
+
+            else if b > 3 && b > a + 1 then
+                WinPlayer2
+
+            else
+                Ongoing ( a, b )
+
+        _ ->
+            game
+
+
+playerScoreToString : Int -> String
+playerScoreToString score =
+    case score of
+        0 ->
+            "Love"
+
+        1 ->
+            "15"
+
+        2 ->
+            "30"
+
+        3 ->
+            "40"
+
+        _ ->
+            "Unhandled"
 
 
 scoreToString : Game -> String
 scoreToString game =
     case game of
-        ( 0, 0 ) ->
-            "Love-Love"
+        WinPlayer1 ->
+            "Player 1 wins"
 
-        ( 1, 0 ) ->
-            "15-Love"
+        WinPlayer2 ->
+            "Player 2 wins"
 
-        ( 0, 1 ) ->
-            "Love-15"
+        AdvantagePlayer1 ->
+            "Advantage Player 1"
 
-        ( 1, 1 ) ->
-            "15-All"
+        AdvantagePlayer2 ->
+            "Advantage Player 2"
 
-        ( 2, 1 ) ->
-            "30-15"
+        Deuce ->
+            "Deuce"
 
-        _ ->
-            "Unhandled score"
+        Ongoing ( a, b ) ->
+            let
+                player1 =
+                    playerScoreToString a
+
+                player2 =
+                    playerScoreToString b
+            in
+            if ( a, b ) == ( 0, 0 ) then
+                "Love-Love"
+
+            else if player1 == player2 then
+                player1 ++ "-All"
+
+            else
+                player1 ++ "-" ++ player2
--- tests/TennisTest.elm
+++ tests/TennisTest.elm
@@ -40,4 +40,113 @@ suite =
                     |> Tennis.player2Scores
                     |> Tennis.scoreToString
                     |> Expect.equal "30-15"
+        , test "player 1 wins after scoring 4 times" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Player 1 wins"
+        , test "player 2 wins after scoring 4 times" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Player 2 wins"
+        , test "player 1 and 2 score 3 times for Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Deuce"
+        , test "player 1 scores after Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Advantage Player 1"
+        , test "player 2 scores after Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Advantage Player 2"
+        , test "player 1 and 2 score after Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Deuce"
+        , test "player 1 wins after Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Player 1 wins"
+        , test "player 1 wins after second Deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Player 1 wins"
+        , test "Scoring after win is not possible" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.scoreToString
+                    |> Expect.equal "Player 1 wins"
         ]
